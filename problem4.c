f132d7e4667caa1d5eb92a847f822734f90a77da
Chirag <chirag.chinvar5@gmail.com>
2021-05-24 10:05:37 +0530

    adding 4th problem

--- problem4.c
+++ problem4.c
@@ -1,3 +1,11 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
 #include <stdio.h>
 
 
@@ -6,17 +14,25 @@ int end=100000000;
 int prime,m,n,sum,count;
 for(int i=0;i<end;i++){
     for(int j=2;j<=i/2;j++){
-        if(i%j==1){
+        if(i%j==0){
             prime=1;
             break;
         }
     }
     if(!prime){
-        m=i%10;    
+        sum=0;
+        m=0;
+        while(i>0){
+            int temp=i;
+        m=temp%10;    
         sum=sum+m;    
-        i=i/10; 
+        temp=temp/10; 
+        }
         if(sum==14)count++;
     }
 }
-
+printf("Count = %d",count);
 }
+
+
+
